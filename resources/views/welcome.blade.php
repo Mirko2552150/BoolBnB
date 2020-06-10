b848b4690c0913d15631b6a18d15e2efb395e834
Mirko <mirko.longo.1987@gmail.com>
2020-06-10 12:35:38 +0200

    Cambio titolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Progetto BoolBnB
                 </div>
 
                 <div class="links">
