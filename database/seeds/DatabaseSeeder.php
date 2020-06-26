9e3e24e367539f1bc8a66a6874eeaf2aae65f92a
Christian Beretta <beretta.christian2602@gmail.com>
2020-06-26 11:55:13 +0200

    database seeder + commento

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // php artisan db:seed per far partire tutti questi seeder a cascata
         $this->call(HomesTableSeeder::class);
+        $this->call(MessagesTableSeeder::class);
+        $this->call(StatsTableSeeder::class);
         $this->call('HomesTableSeeder');
+        // php artisan db:seed --class=StatsTableSeeder per far partire solo il singolo seeder
     }
 }
