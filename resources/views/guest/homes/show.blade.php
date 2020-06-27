c721577a676602fc27ba258269f6436503b4f5e9
Christian Beretta <beretta.christian2602@gmail.com>
2020-06-27 19:58:40 +0200

    UPR che vede propria casa non può inviarsi messaggio

--- resources/views/guest/homes/show.blade.php
+++ resources/views/guest/homes/show.blade.php
@@ -63,32 +63,34 @@
           <input type="search" id="input-map" class="form-control invisible" placeholder="Indirizzo Appartamento"/>
 
         {{-- FORM INVIO DATI CON GET VERSO MESSAGES --}}
-        <form action="{{route('guest.messages.store')}}" method="post">
-          @csrf
-          @method('POST')
-          <div class="form-group">
-              <label for="mail">Email</label>
-              <input type="mail" class="form-control" id="mail" name="mail" placeholder="name@example.com"
-              value="@if (isset($user)){{$user->email}}@endif">
-              @error('mail')
-                  <small class="alert alert-danger form-text text-muted">{{ $message }}</small>
-              @enderror
-          </div>
-          <div class="form-group">
-              <label for="body">Corpo del messaggio</label>
-              <textarea class="form-control" id="body" name="body" rows="3"></textarea>
-              @error('body')
-                  <small class="alert alert-danger form-text text-muted">{{ $message }}</small>
-              @enderror
-          </div>
-          <div class="form-group invisible">
-              <label for="home_id">home_id</label>
-              <input type="number" class="form-control" name="home_id" placeholder="name@example.com" value="{{$home->id}}">
-          </div>
-          <div class="form-group">
-              <input class="btn btn-primary" type="submit" value="Invia messaggio">
-          </div>
-      </form>
+        @if ($user->id !== $home['user_id']) {{-- UPR che ha creato la casa non deve inviarsi un messaggio da solo--}}
+            <form action="{{route('guest.messages.store')}}" method="post">
+              @csrf
+              @method('POST')
+              <div class="form-group">
+                  <label for="mail">Email</label>
+                  <input type="mail" class="form-control" id="mail" name="mail" placeholder="name@example.com"
+                  value="@if (isset($user)){{$user->email}}@endif">
+                  @error('mail')
+                      <small class="alert alert-danger form-text text-muted">{{ $message }}</small>
+                  @enderror
+              </div>
+              <div class="form-group">
+                  <label for="body">Corpo del messaggio</label>
+                  <textarea class="form-control" id="body" name="body" rows="3"></textarea>
+                  @error('body')
+                      <small class="alert alert-danger form-text text-muted">{{ $message }}</small>
+                  @enderror
+              </div>
+              <div class="form-group invisible">
+                  <label for="home_id">home_id</label>
+                  <input type="number" class="form-control" name="home_id" placeholder="name@example.com" value="{{$home->id}}">
+              </div>
+              <div class="form-group">
+                  <input class="btn btn-primary" type="submit" value="Invia messaggio">
+              </div>
+            </form>
+        @endif
 
 
       {{-- INIZIO : passiamo i dati di lat e long alla mappa tramite due input--}}
